5c0e72cf976045a1300ccdfa6f0b8adfec949be6
dinhdeveloper <dinhtrancntt@gmail.com>
2019-01-09 21:33:20 +0700

    chính thức

---
+++ QL_Cafe/src/Interface/Run.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import Models.TaiKhoan;
+
+/**
+ *
+ * @author ThangIKCU
+ */
+public class Run {
+
+    public static frmMain QlCafe;
+    public static frmLogIn frmlg;
+    public static TaiKhoan tk;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        lg();
+    }
+
+    public static void lg() {
+        frmlg = new frmLogIn();
+        frmlg.setVisible(true);
+    }
+
+    public static void QLCF() {
+        QlCafe = new frmMain();
+        QlCafe.setVisible(true);
+    }
+
+}
---
+++ QL_Cafe/src/Models/TaiKhoan.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author TRUNG HÒA
+ */
+public class TaiKhoan {
+    private int id, lv;
+    private String UserName,Password,fullName;
+    public TaiKhoan()
+    {
+        id=lv=0;
+        UserName = "";
+        Password = "";
+    }
+    public TaiKhoan(int id,String Username,String PassWord,int lv,String fullName)
+    {
+        this.id=id;
+        this.lv=lv;
+        this.UserName=Username;
+        this.Password=PassWord;
+        this.fullName = fullName;
+    }
+    public void SetID(int id)
+    {
+        this.id=id;
+    }
+    public int GetID()
+    {
+        return this.id;
+    }
+     public void SetLv(int lv)
+    {
+        this.lv=lv;
+    }
+    public int GetLv()
+    {
+        return this.lv;
+    }
+     public void SetUsername(String Username)
+    {
+        this.UserName=Username;
+    }
+    public String GetUsername()
+    {
+        return this.UserName;
+    }
+     public void SetPassword(String Password)
+    {
+        this.Password=Password;
+    }
+    public String GetPassword()
+    {
+        return this.Password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
+}
